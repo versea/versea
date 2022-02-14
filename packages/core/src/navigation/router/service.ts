c981571811abad870b0fb227b2a4608fd017227f
beefly <1411365142@qq.com>
2022-02-14 14:36:43 +0800

    feat(core): add set router to navigation logic

--- packages/core/src/navigation/router/service.ts
+++ packages/core/src/navigation/router/service.ts
@@ -12,11 +12,20 @@ export * from './interface';
 export class Router implements IRouter {
   protected readonly _matcher: IMatcher;
 
+  /** 标识是否已经给 navigationEvent 传入 router 的实例 */
+  protected hasSetRouter = false;
+
   constructor(@inject(IMatcherKey) matcher: IMatcher) {
     this._matcher = matcher;
   }
 
   public addRoutes(routes: RouteOptions[], app: IApp): void {
+    // 将 router 传给 navigationEvent
+    if (!this.hasSetRouter) {
+      // TODO: setRouter to navigationEvent
+      this.hasSetRouter = true;
+    }
+
     this._matcher.addRoutes(routes, app);
   }
 
