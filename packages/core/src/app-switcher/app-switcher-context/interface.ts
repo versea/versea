4360401b2477463d0eaa8f0287100201a58f97fe
beefly <1411365142@qq.com>
2022-02-21 16:26:55 +0800

    feat(core): add app-switcher comment

--- packages/core/src/app-switcher/app-switcher-context/interface.ts
+++ packages/core/src/app-switcher/app-switcher-context/interface.ts
@@ -10,11 +10,13 @@ export const IAppSwitcherContextKey = createServiceSymbol('IAppSwitcherContext')
 export interface IAppSwitcherContext {
   /**
    * 需要加载的应用
+   * @description 二维数组表示串行和并行，如 [[A], [B, C]] 是先加载 A，再同时加载 B 和 C
    */
   appsToLoad: IApp[][];
 
   /**
    * 需要 mount 的应用
+   * @description 二维数组表示串行和并行，如 [[A], [B, C]] 是先 mount A，再同时 mount B 和 C
    */
   appsToMount: IApp[][];
 
