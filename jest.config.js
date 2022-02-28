6efd85f5afac6b6ae45732e04ea7476dbabd573a
beefly <1411365142@qq.com>
2022-02-28 11:52:42 +0800

    feat: update swc config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   verbose: true,
   transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
+    '^.+\\.(t|j)sx?$': ['@swc/jest'],
   },
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   testEnvironment: 'jsdom',
