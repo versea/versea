67697417b1a6847df4507160b11ccd9cfc8c86f3
kejinzhu <kejinzhu126@126.com>
2022-03-07 22:49:30 +0800

    feat(core): add reroute and navigation events

--- packages/core/src/navigation/router/service.ts
+++ packages/core/src/navigation/router/service.ts
@@ -1,9 +1,14 @@
 import { inject } from 'inversify';
+import queryString from 'query-string';
 
 import { IAppSwitcherKey, IAppSwitcher } from '../../app-switcher/app-switcher/service';
+import { IAppService, IAppServiceKey } from '../../application/app-service/service';
 import { IApp } from '../../application/app/service';
 import { provide } from '../../provider';
 import { IMatcher, IMatcherKey } from '../matcher/service';
+import { setRouter, capturedEventListeners, routingEventsListeningTo } from '../navigation-events';
+import { EventName } from '../navigation-events/types';
+import { MatchedRoute } from '../route/interface';
 import { RouteOptions } from '../route/service';
 import { IRouter, IRouterKey } from './interface';
 
@@ -11,6 +16,8 @@ export * from './interface';
 
 @provide(IRouterKey)
 export class Router implements IRouter {
+  public readonly _appService: IAppService;
+
   protected readonly _matcher: IMatcher;
 
   protected readonly _appSwitcher: IAppSwitcher;
@@ -18,23 +25,57 @@ export class Router implements IRouter {
   /** 标识是否已经给 navigationEvent 传入 router 的实例 */
   protected _hasBindRouter = false;
 
-  constructor(@inject(IMatcherKey) matcher: IMatcher, @inject(IAppSwitcherKey) appSwitcher: IAppSwitcher) {
+  constructor(
+    @inject(IMatcherKey) matcher: IMatcher,
+    @inject(IAppSwitcherKey) appSwitcher: IAppSwitcher,
+    @inject(IAppServiceKey) appService: IAppService,
+  ) {
     this._matcher = matcher;
     this._appSwitcher = appSwitcher;
+    this._appService = appService;
   }
 
   public addRoutes(routes: RouteOptions[], app: IApp): void {
     // 将 router 传给 navigationEvent
     if (!this._hasBindRouter) {
-      // TODO: setRouter to navigationEvent
       this._hasBindRouter = true;
+      setRouter(this);
     }
 
     this._matcher.addRoutes(routes, app);
   }
 
-  // TODO: add match method
-  // public match() {
-  //   return this._matcher.match();
-  // }
+  public match(): MatchedRoute[] {
+    const path: string = location.pathname;
+    const query: queryString.ParsedQuery = queryString.parse(location.search);
+    return this._matcher.match(path, query);
+  }
+
+  public reroute(navigationEvent?: Event): void {
+    const matched = this.match();
+    void this._appSwitcher.switch({
+      routes: matched,
+      navigationEvent,
+    });
+  }
+
+  public callCapturedEventListeners(eventArguments: [Event]): void {
+    if (eventArguments) {
+      const eventType = eventArguments[0].type as EventName;
+      if (routingEventsListeningTo.includes(eventType)) {
+        capturedEventListeners[eventType].forEach((listener: EventListener) => {
+          try {
+            listener.apply(this, eventArguments);
+          } catch (e) {
+            /**
+             * event listener错误不应该中断versea的执行.
+             */
+            setTimeout(() => {
+              throw e;
+            });
+          }
+        });
+      }
+    }
+  }
 }
