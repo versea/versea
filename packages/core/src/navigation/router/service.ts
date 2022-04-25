1652896b517f54f94c23f02c46c70e3406ff9143
shushan.cai <shushan.cai@shopee.com>
2022-04-25 10:48:58 +0800

    feat(core): router match add hash mode

--- packages/core/src/navigation/router/service.ts
+++ packages/core/src/navigation/router/service.ts
@@ -3,6 +3,7 @@ import { parse } from 'query-string';
 
 import { IAppSwitcher, IAppSwitcherKey } from '../../app-switcher/app-switcher/interface';
 import { IApp } from '../../application/app/interface';
+import { IConfigKey, IConfig } from '../../config';
 import { VERSEA_INTERNAL_TAP } from '../../constants';
 import { IHooks, IHooksKey } from '../../hooks/interface';
 import { lazyInject, provide } from '../../provider';
@@ -24,12 +25,19 @@ export class Router implements IRouter {
 
   protected readonly _hooks: IHooks;
 
+  protected readonly _config: IConfig;
+
   /** 标识是否已经把 router 传给 navigation */
   protected _hasBindRouter = false;
 
-  constructor(@inject(IMatcherKey) matcher: IMatcher, @inject(IHooksKey) hooks: IHooks) {
+  constructor(
+    @inject(IMatcherKey) matcher: IMatcher,
+    @inject(IHooksKey) hooks: IHooks,
+    @inject(IConfigKey) config: IConfig,
+  ) {
     this._matcher = matcher;
     this._hooks = hooks;
+    this._config = config;
 
     this._initHooks();
   }
@@ -48,9 +56,17 @@ export class Router implements IRouter {
   }
 
   public match(): MatchedResult {
-    // TODO: handle hash mode
-    const path = window.location.pathname;
-    const query = parse(window.location.search);
+    let path = '';
+    let query = {};
+    if (this._config.routerMode === 'hash') {
+      const wholePath = window.location.hash.substring(1);
+      const queryIndex = wholePath.indexOf('?');
+      path = wholePath.substring(0, queryIndex);
+      query = parse(wholePath.substring(queryIndex + 1));
+    } else {
+      path = window.location.pathname;
+      query = parse(window.location.search);
+    }
     // 匹配路由时，需要再末尾加上 "/"
     return this._matcher.match(path.endsWith('/') ? path : `${path}/`, query);
   }
