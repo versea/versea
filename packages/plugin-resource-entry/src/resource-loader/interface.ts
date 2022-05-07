232ffd7926a5d8e0e1efd328c2f1180bf86ec1fa
beefly <1411365142@qq.com>
2022-05-07 19:49:54 +0800

    feat(plugin-resource-entry): add inline code support

--- packages/plugin-resource-entry/src/resource-loader/interface.ts
+++ packages/plugin-resource-entry/src/resource-loader/interface.ts
@@ -5,4 +5,10 @@ export const IResourceLoaderKey = createServiceSymbol('IResourceLoader');
 export interface IResourceLoader {
   /** 加载资源文件 */
   load: (config: AppConfig) => Promise<AppHooks>;
+
+  isInlineCode: (code: string) => boolean;
+
+  getInlineCode: (code: string) => string;
+
+  evalCode: (src: string, code: string, global: unknown, strict: boolean) => void;
 }
