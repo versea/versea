ccf09bbed7c17a8c550f27714831799df8269eeb
lichao <lichao@shopee.com>
2023-03-10 12:49:14 +0800

    test: add unit test for timeout

--- packages/shared/src/timeout.ts
+++ packages/shared/src/timeout.ts
@@ -10,10 +10,11 @@ export interface RunWithTimeoutOptions {
 export enum TimeoutMethodName {
   LOAD = 'load',
   MOUNT = 'mount',
+  UNMOUNT = 'unmount',
   WAIT_FOR_CHILD_CONTAINER = 'waitForChildContainer',
 }
 
-export type TimeoutConfig = Record<TimeoutMethodName, RunWithTimeoutOptions>;
+export type TimeoutConfig = Partial<Record<TimeoutMethodName, RunWithTimeoutOptions>>;
 
 /**
  * default timeout is 5000ms
@@ -26,7 +27,7 @@ export const promiseWithTimeout = async <T>(
   options: RunWithTimeoutOptions = {},
 ): Promise<Awaited<T>> => {
   // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-  const { maxTime = 5000, dieOnTimeout = true, timeoutMsg } = options || {};
+  const { maxTime = 5000, dieOnTimeout = false, timeoutMsg } = options || {};
   const message = timeoutMsg ?? `Task has been timed out for ${maxTime}.`;
 
   let timer: NodeJS.Timeout | null = null;
@@ -60,18 +61,18 @@ export const promiseWithTimeout = async <T>(
 };
 
 export function timeout(options: RunWithTimeoutOptions & { configName?: TimeoutMethodName } = {}) {
-  return function (_target: Record<string, unknown>, methodName: string, descriptor: PropertyDescriptor): void {
+  return function (_target: unknown, methodName: string, descriptor: PropertyDescriptor): void {
     const originValue = descriptor.value as (...args: unknown[]) => Promise<unknown>;
 
-    const config = (_target._timeoutConfig ?? {}) as TimeoutConfig;
-
-    // options priority: options -> options from config by given configName -> options from config by the raw method name
-    const finalOptions = {
-      ...(config[(options?.configName ?? methodName) as TimeoutMethodName] ?? {}),
-      ...options,
-    };
-
     descriptor.value = async function (...args: unknown[]): Promise<unknown> {
+      const config = (this as { _timeoutConfig?: TimeoutConfig })._timeoutConfig ?? {};
+
+      // options priority: options -> options from config by given configName -> options from config by the raw method name
+      const finalOptions = {
+        ...(config[(options?.configName ?? methodName) as TimeoutMethodName] ?? {}),
+        ...options,
+      };
+
       const taskPromise = originValue.apply(this, args);
       return promiseWithTimeout(taskPromise, finalOptions);
     };
